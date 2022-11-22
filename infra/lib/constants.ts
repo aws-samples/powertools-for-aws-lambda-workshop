af03c41249d87240bd0eeef41bbbc08dfa3d7b83
Sara Gerion <sgerion@amazon.nl>
2022-11-22 22:37:46 +0100

    costant update + frontend build

--- infra/lib/constants.ts
+++ infra/lib/constants.ts
@@ -3,22 +3,18 @@ import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Runtime, Tracing, FunctionProps } from 'aws-cdk-lib/aws-lambda';
 import { BundlingOptions } from 'aws-cdk-lib/aws-lambda-nodejs';
 
-export const environment =
-  process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+export const environment = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
 
-export const dynamoFilesTableName = `FilesTable-${environment}`;
+export const dynamoFilesTableName = `media-processing-app-files-${environment}`;
 export const dynamoFilesByUserGsiName = 'filesByUserIndex';
 
 export const websiteBucketNamePrefix = 'website';
 export const landingZoneBucketNamePrefix = 'landing-zone';
 
-export const powertoolsServiceName =
-  'aws-lambda-powertools-typescript-workshop';
-export const powertoolsLoggerLogLevel =
-  process.env.NODE_ENV === 'production' ? 'WARN' : 'DEBUG';
-export const powertoolsLoggerSampleRate =
-  process.env.NODE_ENV === 'production' ? '0.1' : '1';
-export const powertoolsMetricsNamespace = 'octank'; // Dummy company name
+export const powertoolsServiceName = 'media-processing-app';
+export const powertoolsLoggerLogLevel = process.env.NODE_ENV === 'production' ? 'WARN' : 'DEBUG';
+export const powertoolsLoggerSampleRate = process.env.NODE_ENV === 'production' ? '0.1' : '1';
+export const powertoolsMetricsNamespace = 'AnyCompany'; // Dummy company name
 
 export const trafficGeneratorIntervalInMinutes = 1;
 
