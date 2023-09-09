bcc6c719273b517597b152cafb7fb47ac76a5a6b
Leandro Damascena <lcdama@amazon.pt>
2023-09-09 12:44:03 +0100

    Fixing error function 1

--- functions/python/modules/module1/app.py
+++ functions/python/modules/module1/app.py
@@ -2,13 +2,17 @@ import boto3
 import uuid
 from utils import (
     get_image_metadata,
+    get_original_object,
+    create_thumbnail,
+    write_thumbnail_to_s3,
     s3_bucket_files,
     files_table_name,
     idempotency_table_name,
     region_name
 )
-from constants import TRANSFORMED_IMAGE_PREFIX, TRANSFORMED_IMAGE_EXTENSION
+from constants import TRANSFORMED_IMAGE_PREFIX, TRANSFORMED_IMAGE_EXTENSION, TRANSFORM_SIZE
 from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.idempotency import idempotent_function, DynamoDBPersistenceLayer, IdempotencyConfig
 # Data class - Use this?
@@ -16,6 +20,7 @@ from aws_lambda_powertools.utilities.idempotency import idempotent_function, Dyn
 
 # DynamoDB client
 dynamodb_client = boto3.client("dynamodb", region_name=region_name)
+s3_client = boto3.client("s3", region_name=region_name)
 
 logger = Logger()
 metrics = Metrics(namespace="workshop-opn301")
@@ -24,23 +29,39 @@ tracer = Tracer()
 # Change table name? Get from ENV?
 persistence_layer = DynamoDBPersistenceLayer(table_name=idempotency_table_name)
 idempotency_config = IdempotencyConfig(
-    event_key_jmespath='[etag,userId]',
+    event_key_jmespath='detail.object.[etag,userId]',
     raise_on_no_idempotency_key=True,
     expires_after_seconds=60 * 60 * 2, # 2 hours
 )
 
 
-#@idempotent_function
-def process_one(object_key: str):
-    new_object_key: str = f"{TRANSFORMED_IMAGE_PREFIX}/{uuid.uuid4()}/{TRANSFORMED_IMAGE_PREFIX}"
+@idempotent_function(persistence_store=persistence_layer, config=idempotency_config, data_keyword_argument="event")
+def process_thumbnail(event: dict, object_key: str, object_etag:str):
+    new_thumbnail_key: str = f"{TRANSFORMED_IMAGE_PREFIX}/{uuid.uuid4()}{TRANSFORMED_IMAGE_EXTENSION}"
+
+    logger.info(f"Generate Thumbnail for Object Key: {object_key} and Etag: {object_etag}");
+
+    original_image: bytes = get_original_object(s3_client=s3_client,object_key=object_key,bucket_name=s3_bucket_files)
+
+    thumbnail_size = TRANSFORM_SIZE.get("SMALL")
+
+    thumbnail_image = create_thumbnail(original_image=original_image, width=thumbnail_size.get("width"), height=thumbnail_size.get("height"))
+
+    write_thumbnail_to_s3(s3_client=s3_client, object_key=new_thumbnail_key, bucket_name=s3_bucket_files,body=thumbnail_image)
+
+    logger.info('Saved image on S3', detail=new_thumbnail_key);
+
+    metrics.add_metric(name='processedImages', unit=MetricUnit.Count, value=1)
+
+    return new_thumbnail_key
 
 
-# Data class - Use this?
-#@event_source(data_class=EventBridgeEvent)
+
 @tracer.capture_lambda_handler
 @metrics.log_metrics(capture_cold_start_metric=True)
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context: LambdaContext):
+
     # Register Lambda context to handle potential timeouts
     idempotency_config.register_lambda_context(context)
 
@@ -55,5 +76,12 @@ def lambda_handler(event, context: LambdaContext):
 
     # Add GraphQL API
     # Mark file as working, this will notify subscribers that the file is being processed
-    print(image_metadata)
-    print(event)
+
+    # try to process file using idempotency
+    try:
+        thumbnail_object_key = process_thumbnail(event=event, object_key=object_key, object_etag=object_etag)
+
+        # ADD GRAPHQL API
+    except Exception as exc:
+        logger.exception('An unexpected error occurred', log=exc)
+        raise Exception(exc)
--- functions/python/modules/module1/constants.py
+++ functions/python/modules/module1/constants.py
@@ -1,2 +1,8 @@
 TRANSFORMED_IMAGE_PREFIX = "transformed/image/jpg"
 TRANSFORMED_IMAGE_EXTENSION = ".jpeg"
+
+TRANSFORM_SIZE = {
+    "SMALL": {"width": 720, "height": 480},
+    "MEDIUM": {"width": 1280, "height": 720},
+    "LARGE": {"width": 1920, "height": 1080},
+}
