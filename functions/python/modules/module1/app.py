3dd334addfd5130e25e56f65fe6ab15130de2f74
Leandro Damascena <lcdama@amazon.pt>
2023-09-10 12:21:30 +0100

    Formatting file

--- functions/python/modules/module1/app.py
+++ functions/python/modules/module1/app.py
@@ -8,15 +8,24 @@ from utils import (
     s3_bucket_files,
     files_table_name,
     idempotency_table_name,
-    region_name
+    region_name,
 )
-from constants import TRANSFORMED_IMAGE_PREFIX, TRANSFORMED_IMAGE_EXTENSION, TRANSFORM_SIZE
+from constants import (
+    TRANSFORMED_IMAGE_PREFIX,
+    TRANSFORMED_IMAGE_EXTENSION,
+    TRANSFORM_SIZE,
+)
 from aws_lambda_powertools import Logger, Metrics, Tracer
 from aws_lambda_powertools.metrics import MetricUnit
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.idempotency import idempotent_function, DynamoDBPersistenceLayer, IdempotencyConfig
+from aws_lambda_powertools.utilities.idempotency import (
+    idempotent_function,
+    DynamoDBPersistenceLayer,
+    IdempotencyConfig,
+)
+
 # Data class - Use this?
-#from aws_lambda_powertools.utilities.data_classes import event_source, EventBridgeEvent
+# from aws_lambda_powertools.utilities.data_classes import event_source, EventBridgeEvent
 
 # DynamoDB client
 dynamodb_client = boto3.client("dynamodb", region_name=region_name)
@@ -29,32 +38,50 @@ tracer = Tracer()
 # Change table name? Get from ENV?
 persistence_layer = DynamoDBPersistenceLayer(table_name=idempotency_table_name)
 idempotency_config = IdempotencyConfig(
-    event_key_jmespath='detail.object.[etag,userId]',
+    event_key_jmespath="detail.object.[etag,userId]",
     raise_on_no_idempotency_key=True,
-    expires_after_seconds=60 * 60 * 2, # 2 hours
+    expires_after_seconds=60 * 60 * 2,  # 2 hours
 )
 
 
-@idempotent_function(persistence_store=persistence_layer, config=idempotency_config, data_keyword_argument="event")
-def process_thumbnail(event: dict, object_key: str, object_etag:str):
-    new_thumbnail_key: str = f"{TRANSFORMED_IMAGE_PREFIX}/{uuid.uuid4()}{TRANSFORMED_IMAGE_EXTENSION}"
+@idempotent_function(
+    persistence_store=persistence_layer,
+    config=idempotency_config,
+    data_keyword_argument="event",
+)
+def process_thumbnail(event: dict, object_key: str, object_etag: str):
+    new_thumbnail_key: str = (
+        f"{TRANSFORMED_IMAGE_PREFIX}/{uuid.uuid4()}{TRANSFORMED_IMAGE_EXTENSION}"
+    )
 
-    logger.info(f"Generate Thumbnail for Object Key: {object_key} and Etag: {object_etag}");
+    logger.info(
+        f"Generate Thumbnail for Object Key: {object_key} and Etag: {object_etag}"
+    )
 
-    original_image: bytes = get_original_object(s3_client=s3_client,object_key=object_key,bucket_name=s3_bucket_files)
+    original_image: bytes = get_original_object(
+        s3_client=s3_client, object_key=object_key, bucket_name=s3_bucket_files
+    )
 
     thumbnail_size = TRANSFORM_SIZE.get("SMALL")
 
-    thumbnail_image = create_thumbnail(original_image=original_image, width=thumbnail_size.get("width"), height=thumbnail_size.get("height"))
+    thumbnail_image = create_thumbnail(
+        original_image=original_image,
+        width=thumbnail_size.get("width"),
+        height=thumbnail_size.get("height"),
+    )
 
-    write_thumbnail_to_s3(s3_client=s3_client, object_key=new_thumbnail_key, bucket_name=s3_bucket_files,body=thumbnail_image)
+    write_thumbnail_to_s3(
+        s3_client=s3_client,
+        object_key=new_thumbnail_key,
+        bucket_name=s3_bucket_files,
+        body=thumbnail_image,
+    )
 
-    logger.info('Saved image on S3', detail=new_thumbnail_key);
+    logger.info("Saved image on S3", detail=new_thumbnail_key)
 
-    metrics.add_metric(name='processedImages', unit=MetricUnit.Count, value=1)
+    metrics.add_metric(name="processedImages", unit=MetricUnit.Count, value=1)
 
     return new_thumbnail_key
-
 
 
 @tracer.capture_lambda_handler
@@ -70,7 +97,11 @@ def lambda_handler(event, context: LambdaContext):
     object_key: str = event.get("detail", {}).get("object", {}).get("key")
     object_etag: str = event.get("detail", {}).get("object", {}).get("etag")
 
-    image_metadata = get_image_metadata(dynamodb_client=dynamodb_client, table_name=files_table_name, object_key=object_key)
+    image_metadata = get_image_metadata(
+        dynamodb_client=dynamodb_client,
+        table_name=files_table_name,
+        object_key=object_key,
+    )
     file_id = image_metadata["fileId"]["S"]
     user_id = image_metadata["userId"]["S"]
 
@@ -79,9 +110,11 @@ def lambda_handler(event, context: LambdaContext):
 
     # try to process file using idempotency
     try:
-        thumbnail_object_key = process_thumbnail(event=event, object_key=object_key, object_etag=object_etag)
+        thumbnail_object_key = process_thumbnail(
+            event=event, object_key=object_key, object_etag=object_etag
+        )
 
         # ADD GRAPHQL API
     except Exception as exc:
-        logger.exception('An unexpected error occurred', log=exc)
+        logger.exception("An unexpected error occurred", log=exc)
         raise Exception(exc)
--- functions/python/modules/module1/utils.py
+++ functions/python/modules/module1/utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 from PIL import Image
 import io
+
 # Should we add stubs to make easy typing?
 
 s3_bucket_files = os.getenv("BUCKET_NAME_FILES", "aaaabbcccdddd")
@@ -12,45 +13,45 @@ region_name = os.getenv("AWS_REGION", "us-east-1")
 
 
 def extract_file_id(object_key: str) -> str:
-    return object_key.split('/')[-1].split('.')[0]
+    return object_key.split("/")[-1].split(".")[0]
+
 
 def get_image_metadata(dynamodb_client, table_name: str, object_key: str):
     try:
         response = dynamodb_client.get_item(
             TableName=table_name,
-            Key={
-                'id': {"S": extract_file_id(object_key)}
-            },
-            ProjectionExpression='id, userId'
+            Key={"id": {"S": extract_file_id(object_key)}},
+            ProjectionExpression="id, userId",
         )
 
-        if 'Item' not in response:
-            raise Exception('File metadata not found')
+        if "Item" not in response:
+            raise Exception("File metadata not found")
 
-        return {
-            'fileId': response['Item']['id'],
-            'userId': response['Item']['userId']
-        }
+        return {"fileId": response["Item"]["id"], "userId": response["Item"]["userId"]}
     except Exception as e:
         raise e
 
+
 def get_original_object(s3_client, object_key: str, bucket_name: str):
-    
+
     try:
         response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-        body = response['Body']
-        
+        body = response["Body"]
+
         chunks = []
         while True:
-            data = body.read(1024) #1kb I think it's ok
+            data = body.read(1024)  # 1kb I think it's ok
             if not data:
                 break
             chunks.append(data)
-        
+
         print("AQUI")
-        return b''.join(chunks)
+        return b"".join(chunks)
     except Exception as e:
-        raise Exception(f"Error getting file from S3 -> {str(e)}")  # Handle or log the exception as needed
+        raise Exception(
+            f"Error getting file from S3 -> {str(e)}"
+        )  # Handle or log the exception as needed
+
 
 def create_thumbnail(original_image: bytes, width: int, height: int) -> bytes | None:
     try:
@@ -70,7 +71,9 @@ def create_thumbnail(original_image: bytes, width: int, height: int) -> bytes | 
         return None
 
 
-def write_thumbnail_to_s3(s3_client, object_key: str, bucket_name: str, body: bytes=None):
+def write_thumbnail_to_s3(
+    s3_client, object_key: str, bucket_name: str, body: bytes = None
+):
     try:
         file_body = body
         # Upload the object to the specified S3 bucket
