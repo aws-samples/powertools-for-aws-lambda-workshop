40008d569bf8f9b621ab4d1edae9c4fe69b4064e
Andrea Amorosi <dreamorosi@gmail.com>
2023-10-26 15:01:14 +0200

    added lb + cf

--- infra/lib/attendant-ide/index.ts
+++ infra/lib/attendant-ide/index.ts
@@ -1,4 +1,4 @@
-import { CfnOutput, Stack, Tags } from 'aws-cdk-lib';
+import { CfnOutput, Duration, Stack, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {
   Vpc,
@@ -9,8 +9,34 @@ import {
   MachineImage,
   BlockDeviceVolume,
   UserData,
+  Port,
+  Peer,
+  FlowLogDestination,
+  FlowLogTrafficType,
 } from 'aws-cdk-lib/aws-ec2';
-import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import {
+  LoadBalancer,
+  LoadBalancingProtocol,
+} from 'aws-cdk-lib/aws-elasticloadbalancing';
+import { AutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
+import { ManagedPolicy, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import {
+  AllowedMethods,
+  CacheCookieBehavior,
+  CachePolicy,
+  CachedMethods,
+  Distribution,
+  ViewerProtocolPolicy,
+} from 'aws-cdk-lib/aws-cloudfront';
+import {
+  LoadBalancerV2Origin,
+  S3Origin,
+} from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Role } from 'aws-cdk-lib/aws-iam';
+import { ParameterDataType, StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { environment } from '../constants';
+import { randomUUID } from 'crypto';
 
 interface AttendantIdeProps {}
 
@@ -18,10 +44,31 @@ export class AttendantIde extends Construct {
   public constructor(scope: Construct, id: string, _props: AttendantIdeProps) {
     super(scope, id);
 
-    const defaultVpc = Vpc.fromLookup(this, 'defaultVpc', { isDefault: true });
+    // const defaultVpc = Vpc.fromLookup(this, 'defaultVpc', { isDefault: true });
+    const vpc = new Vpc(this, 'VPC', {
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+      flowLogs: {
+        VPCFlowLogs: {
+          destination: FlowLogDestination.toCloudWatchLogs(),
+          trafficType: FlowLogTrafficType.REJECT,
+        },
+      },
+    });
     const instanceSecurityGroup = new SecurityGroup(this, 'instance-sg', {
-      vpc: defaultVpc,
+      vpc,
       allowAllOutbound: true,
+    });
+
+    const idePasswordParameter = new StringParameter(this, 'ide-password', {
+      dataType: ParameterDataType.TEXT,
+      stringValue: randomUUID(),
     });
 
     const userData = UserData.forLinux();
@@ -56,7 +103,7 @@ export class AttendantIde extends Construct {
       'ln -s /opt/apache-maven-3.9.5/ /opt/maven',
       'echo "export M2_HOME=/opt/maven" | tee /etc/profile.d/mvn.sh',
       'echo "export PATH=${M2_HOME}/bin:${PATH}" | tee -a /etc/profile.d/mvn.sh',
-      'chmod a+x /etc/profile.d/mvn.sh'
+      'chmod a+x /etc/profile.d/mvn.sh',
       // Install AWS CLI
       "su - ec2-user -c 'curl https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip -o $HOME/awscliv2.zip'",
       "su - ec2-user -c 'unzip $HOME/awscliv2.zip'",
@@ -67,39 +114,116 @@ export class AttendantIde extends Construct {
       // Configure git
       `su - ec2-user -c 'git config --global init.defaultBranch main'`,
       // Install VSCode
-      "su - ec2-user -c 'curl -fsSL https://code-server.dev/install.sh | sh'"
+      "su - ec2-user -c 'curl -fsSL https://code-server.dev/install.sh | sh'",
+      'systemctl enable --now code-server@ec2-user',
+      "sed -i 's/127.0.0.1/0.0.0.0/g' /home/ec2-user/.config/code-server/config.yaml",
+      'reboot'
     );
 
-    const vscodeInstance = new Instance(this, 'instance', {
-      vpc: defaultVpc,
-      vpcSubnets: { subnetType: SubnetType.PUBLIC },
-      instanceType: new InstanceType('c6g.large'),
-      machineImage: MachineImage.fromSsmParameter(
-        '/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-arm64'
-      ),
-      securityGroup: instanceSecurityGroup,
-      keyName: 'VSCode',
-      blockDevices: [
+    const vscodeInstanceRole = new Role(this, 'vscode-instance-role', {
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AmazonEC2RoleforSSM'
+        ),
+      ],
+    });
+    idePasswordParameter.grantRead(vscodeInstanceRole);
+
+    const vscodeLoadBalancer = new LoadBalancer(this, 'vscode-lb', {
+      vpc,
+      internetFacing: true,
+      healthCheck: {
+        port: 8080,
+        path: '/healthz',
+      },
+      listeners: [
         {
-          deviceName: '/dev/xvda',
-          volume: BlockDeviceVolume.ebs(50),
+          externalPort: 443,
+          externalProtocol: LoadBalancingProtocol.HTTPS,
+          sslCertificateArn: new Certificate(this, 'vscode-cert', {
+            domainName: 'vscode.chronicled.com',
+          }).certificateArn,
         },
       ],
-      userDataCausesReplacement: true,
-      userData: userData,
+      targets: [
+        new AutoScalingGroup(this, 'vscode-asg', {
+          vpc,
+          maxCapacity: 1,
+          minCapacity: 1,
+          instanceType: new InstanceType('c6g.large'),
+          machineImage: MachineImage.fromSsmParameter(
+            '/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64'
+          ),
+          userData,
+          /* blockDevices: [
+            {
+              deviceName: '/dev/xvda',
+              volume: BlockDeviceVolume.ebs(50),
+            },
+          ], */
+          securityGroup: instanceSecurityGroup,
+          // keyName: TODO: check WS key name
+          role: vscodeInstanceRole,
+        }),
+      ],
     });
-    Tags.of(vscodeInstance).add('chronicled', 'true');
-    Tags.of(vscodeInstance).add('Name', 'VSCode');
-    vscodeInstance.role.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+
+    instanceSecurityGroup.addIngressRule(
+      Peer.securityGroupId(
+        vscodeLoadBalancer.loadBalancerSourceSecurityGroupGroupName
+      ),
+      Port.tcp(8080),
+      'allows lb'
     );
 
-    new CfnOutput(this, 'instanceId', {
+    const distribution = new Distribution(this, 'distribution', {
+      defaultBehavior: {
+        // TODO set origin
+        // origin: new LoadBalancerOrigin(vscodeLoadBalancer),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachedMethods: CachedMethods.CACHE_GET_HEAD_OPTIONS,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        cachePolicy: new CachePolicy(this, 's3-cache', {
+          cachePolicyName: `ide-cache-${environment}`,
+          minTtl: Duration.seconds(0),
+          maxTtl: Duration.seconds(86400),
+          defaultTtl: Duration.seconds(86400),
+          cookieBehavior: CacheCookieBehavior.none(),
+          enableAcceptEncodingGzip: true,
+        }),
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+      enableIpv6: true,
+      enabled: true,
+    });
+
+    new CfnOutput(this, 'IDEWorkspace', {
+      value: distribution.distributionDomainName,
+      description: 'The domain name where the website is hosted',
+    });
+
+    /* new CfnOutput(this, 'instanceId', {
       value: vscodeInstance.instanceId,
     });
 
     new CfnOutput(this, 'publicIP', {
       value: vscodeInstance.instancePublicIp,
+    }); */
+    new CfnOutput(this, 'loadBalancerIp', {
+      value: vscodeLoadBalancer.loadBalancerDnsName,
     });
   }
 }
