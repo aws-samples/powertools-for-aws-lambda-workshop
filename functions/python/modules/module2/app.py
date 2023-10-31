83602d6ed1379200d1aa5e4a0cd0b3bb759ae570
Leandro Damascena <lcdama@amazon.pt>
2023-10-31 17:49:15 +0000

    Refactoring

--- functions/python/modules/module2/app.py
+++ functions/python/modules/module2/app.py
@@ -19,8 +19,10 @@ API_KEY_SECRET_NAME = os.getenv("API_KEY_SECRET_NAME", "")
 
 secrets = boto3.client("secretsmanager")
 
+def get_secret_value(secret_id: str):
+    return secrets.get_secret_value(SecretId=secret_id).get("SecretString")
 
-@tracer.capture_method
+
 def record_handler(record: DynamoDBRecord):
 
     # Since we are applying the filter at the DynamoDB Stream level,
@@ -29,21 +31,18 @@ def record_handler(record: DynamoDBRecord):
     transformed_key = record.dynamodb.new_image.get("transformedFileKey")
     file_id = record.dynamodb.new_image.get("id")
 
-    with tracer.provider.in_subsegment("## get_labels") as subsegment:
-        try:
-            get_labels(S3_BUCKET_FILES, file_id, user_id, transformed_key)
-        except (NoLabelsFoundError, NoPersonFoundError):
-            logger.warning("No person found in the image")
-            # Get the apiUrl and apiKey
-            # You can replace these with the actual values or retrieve them from a secret manager.
-            api_url = json.loads(API_URL_HOST).get("url")
-            api_key = secrets.get_secret_value(SecretId=API_KEY_SECRET_NAME).get("SecretString")
-            if not api_key:
-                raise Exception(f"Unable to get secret {api_key}")
-            
-            report_image_issue(file_id=file_id, user_id=user_id, api_key=api_key, api_url=api_url, logger=logger)
-        except ImageDetectionError as error:
-            logger.error(error)
+    try:
+        get_labels(S3_BUCKET_FILES, file_id, user_id, transformed_key)
+    except (NoLabelsFoundError, NoPersonFoundError):
+        logger.warning("No person found in the image")
+        api_url = json.loads(API_URL_HOST).get("url")
+        api_key = get_secret_value(API_KEY_SECRET_NAME)
+        if not api_key:
+            raise Exception(f"Unable to get secret {api_key}")
+        
+        report_image_issue(file_id=file_id, user_id=user_id, api_key=api_key, api_url=api_url, logger=logger)
+    except ImageDetectionError as error:
+        logger.error(error)
 
 @tracer.capture_lambda_handler
 @logger.inject_lambda_context(log_event=True)
